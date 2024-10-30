fe3ba7f1615e9eae019dcc137f834026e9c96e2a
Sinan Turan <turansinan2004@gmail.com>
2024-10-30 20:24:16 +0300

    CategoryService added

---
+++ src/main/java/com/example/hand_in_hand/service/CategoryServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.hand_in_hand.service;
+
+import com.example.hand_in_hand.dao.CategoryDAOImpl;
+import com.example.hand_in_hand.entities.models.Category;
+import com.example.hand_in_hand.service.Contracts.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    private CategoryDAOImpl categoryDAO;
+    @Autowired
+    public CategoryServiceImpl(CategoryDAOImpl categoryDAO) {
+        this.categoryDAO = categoryDAO;
+    }
+    @Transactional
+    @Override
+    public Category save(Category entity) {
+        return categoryDAO.save(entity);
+    }
+
+    @Override
+    public List getAll() {
+        return categoryDAO.getAll();
+    }
+
+    @Override
+    public Category getById(int id) {
+        return categoryDAO.getById(id);
+    }
+    @Transactional
+    @Override
+    public void update(Category entity) {
+        categoryDAO.update(entity);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int id) {
+        categoryDAO.deleteById(id);
+    }
+}
